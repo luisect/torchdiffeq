652077aafe53756dfd4274e3fe0b1e9a879b4264
luisect <luis.ect@gmail.com>
2020-12-01 07:20:04 -0500

    Update odeint.py

--- torchdiffeq/_impl/odeint.py
+++ torchdiffeq/_impl/odeint.py
@@ -26,13 +26,13 @@ SOLVERS = {
 
 def odeint(func, y0, t, rtol=1e-7, atol=1e-9, method=None, options=None):
    
-    #shapes, func, y0, t, rtol, atol, method, options = _check_inputs(func, y0, t, rtol, atol, method, options, SOLVERS)
+    shapes, func, y0, t, rtol, atol, method, options = _check_inputs(func, y0, t, rtol, atol, method, options, SOLVERS)
 
-    #solver = SOLVERS[method](func=func, y0=y0, rtol=rtol, atol=atol, **options)
-    #solution = solver.integrate(t)
+    solver = SOLVERS[method](func=func, y0=y0, rtol=rtol, atol=atol, **options)
+    solution = solver.integrate(t)
         
-    #if shapes is not None:
-    #    solution = _flat_to_shape(solution, (len(t),), shapes)
+    if shapes is not None:
+        solution = _flat_to_shape(solution, (len(t),), shapes)
     return solution 
 
  
